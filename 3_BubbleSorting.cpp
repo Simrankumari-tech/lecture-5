52d638fcc75e2b94a79ac56d80e9b3ee8b3035e4
Simran kumari <simrankumari.nea10b@gmail.com>
2024-07-27 12:13:25 +0530

    Update 3_BubbleSorting.cpp

--- 3_BubbleSorting.cpp
+++ 3_BubbleSorting.cpp
@@ -4,7 +4,7 @@ int main(){
 int a[] ={6,0,3,2};
 int n= sizeof(a)/ sizeof(int);
 for(int i= 0; i<n-1;i++){
-    for(int j=0; j<n-2-i ;j++){
+    for(int j=0; j<n-1-i ;j++){
         if(a[j] > a[j+1]){
             swap (a[j], a[j+1]);
         }
